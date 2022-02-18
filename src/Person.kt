b330cd08a970ddc86e15e096eb8595d77e3651a7
ilyabrous <56649640+ilyabrous@users.noreply.github.com>
2022-02-18 23:29:31 +0300

    Update Person.kt

--- src/Person.kt
+++ src/Person.kt
@@ -3,10 +3,10 @@ import kotlin.random.Random
 data class PersonAuth(val email: String, val password: String)
 data class PersonRegister(val name: String, val email: String, val password: String)
 
-fun authPerson(personAuth: PersonAuth) : Boolean = Random.nextBoolean()
+fun authPersonFork(personAuth: PersonAuth) : Boolean = Random.nextBoolean()
 
 fun changePersonPassword(email: String, newPassword: String) : Boolean = Random.nextBoolean()
 
 fun registerPerson(personRegister: PersonRegister): Boolean = Random.nextBoolean()
 
-fun deleteUser(email: String) : Boolean = Random.nextBoolean()
+fun deleteUserFork(email: String) : Boolean = Random.nextBoolean()
