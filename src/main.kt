67df7a59a41a7f94872c7d89ba8ef69ad444c796
Ilya Grodnev <ilyabrous@gmail.com>
2022-02-18 23:27:14 +0300

    some changes in main

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,8 @@
 fun main() {
-    val personRegister = PersonRegister("Ilya","google@google.by", "bestHash")
+    val personRegister = PersonRegister("Andrew","google@google.by", "bestHash")
     val personAuth = PersonAuth(personRegister.name, personRegister.password)
     authPerson(personAuth)
     changePersonPassword(personAuth.email, "md5Hash")
     deleteUser(personAuth.email)
+
 }
